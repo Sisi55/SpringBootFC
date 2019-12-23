7756c0c7228e036c41886afa26bf21d3dea32fa0
Sisi <dqgsh1055@gmail.com>
2019-12-24 03:40:55 +0900

    Initial commit

---
+++ src/test/java/com/example/fcstudy/FcstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fcstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
